a50b471eb71a64649097107120de43e5d29c4207
vdsou <vdsouza@outlook.com>
2021-10-06 12:22:53 -0300

    header and banner done

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Banner.css';
+
+export default function Banner() {
+  return (
+    <section
+      className="banner"
+      id="banner"
+      style={{
+        background: 'url("../image/banner-bg.webp") no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="content">
+        <h3>
+          fresh and
+          {' '}
+          <span>organic</span>
+          {' '}
+          products
+        </h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
+          vitae perspiciatis neque soluta.
+        </p>
+        <button type="button" className="btn">
+          shop now
+        </button>
+      </div>
+    </section>
+  );
+}
