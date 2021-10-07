ab40242b7aba09bed50dd87d64fefb52576f3cfe
vdsou <vdsouza@outlook.com>
2021-10-07 14:10:37 -0300

    fix autoplay in reviews

--- src/components/Reviews/index.jsx
+++ src/components/Reviews/index.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import SwiperCore, { Autoplay } from 'swiper';
 import './Reviews.css';
 import 'swiper/swiper.min.css';
 
 export default function Reviews() {
+  SwiperCore.use([Autoplay]);
   return (
     <section className="reviews" id="reviews">
       <h1 className="heading">
@@ -17,11 +19,21 @@ export default function Reviews() {
       <div className="reviews-slider">
         <Swiper
           loop
+          autoplay={{ delay: 7500, disableOnInteraction: false }}
           slidesPerView={3}
           centeredSlides
           spaceBetween={20}
           pagination={{ clickable: true }}
           style={{ padding: '1rem' }}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: { slidesPerView: 3 },
+          }}
         >
           <SwiperSlide>
             <div className="box">
